dae3039801300cd82a814301b3c180aa983a21fb
Ryan Mwangi <ryanmwangi61@gmail.com>
2023-08-22 16:09:21 +0000

    Produce output according to a format

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
 /**
  * _printf - Prints any character
  * @format: Pointer to char
@@ -13,30 +12,31 @@ int _printf(const char *format, ...)
 	va_list mylist;
 
 	va_start(mylist, format);
-
-	for (a = 0; format[a] != '\0'; a++)
+	if (format != NULL)
 	{
-		if (format[a] != '%')
+		for (a = 0; format[a] != '\0'; a++)
 		{
-			pu_char(format[a]);
+			if (format[a] != '%')
+			{
+				pu_char(format[a]);
+			}
+			else if (format[a + 1] == 'c')
+			{
+				pu_char(va_arg(mylist, int));
+				a++;
+			}
+			else if (format[a + 1] == 's')
+			{
+				s_count = putss((va_arg(mylist, char *)));
+				a++;
+				count += (s_count - 1);
+			}
+			else if (format[a + 1] == '%')
+			{
+				pu_char('%');
+			}
+			count += 1;
 		}
-		else if (format[a + 1] == 'c')
-		{
-			pu_char(va_arg(mylist, int));
-					a++;
-		}
-		else if (format[a + 1] == 's')
-		{
-			s_count = putss((va_arg(mylist, char *)));
-			a++;
-			count += (s_count - 1);
-		}
-		else if (format[a + 1] == '%')
-		{
-			pu_char('%');
-		}
-		count += 1;
-	}
-	va_end(mylist);
-	return (count);
+		va_end(mylist);
+	}	return (count);
 }
--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
+#include <stddef.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
 int putss(char *c);
 int pu_char(char c);
